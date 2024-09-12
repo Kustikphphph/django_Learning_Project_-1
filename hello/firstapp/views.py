d5433975f5001d05aa80f26e63ca3963329fe848
Your Name <you@example.com>
2024-09-12 12:33:15 +0300

    12.09.24. Статичные файлы Ростовцева Шевелева

--- hello/firstapp/views.py
+++ hello/firstapp/views.py
@@ -5,14 +5,7 @@ from django.template.response import TemplateResponse
 # Create your views here.
 
 def index(request):
-    header = "Персональные данные" # обычная переменная
-    langs = ["Английский", "Немецкий", "Испанский"] # массив
-    user = {"name": "Максим,", "age": 30} # словарь
-    addr = ("Виноградная", 23, 45) # кортеж
-    data = {"header": header, "langs": langs, "user": user, "address":
-    addr}
-    return TemplateResponse(request, "index.html", data)
-
+    return render(request, "firstapp/index.html")
 def about(request):
     return HttpResponse("About")
 def contact(request):
