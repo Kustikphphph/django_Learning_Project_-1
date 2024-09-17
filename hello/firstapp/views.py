3325f5bbb72e786cb7bb2710e9f35e7c8df21851
Your Name <you@example.com>
2024-09-17 12:36:43 +0300

    17.09.24  Настройка формы и ее полей, изменение внешнего вида поля с помощью параметра widget,Задание начальных значений полей (initial), field_order -порядок полей на форме, help_text, nастройки вида формы, проверка (валидация) данных,стилu полeu формы Шевелева Ростовцева

--- hello/firstapp/views.py
+++ hello/firstapp/views.py
@@ -9,6 +9,11 @@ from .forms import UserForm
 
 def index(request):
     userform = UserForm()
+    if request.method == "POST":
+        userform = UserForm(request.POST)
+        if userform.is_valid():
+            name = userform.cleaned_data["name"]
+            return HttpResponse("<h2>Имя введено коррректно – {0}</h2>".format(name))
     return render(request, "firstapp/index.html", {"form": userform})
 def about(request):
     return HttpResponse("About")
