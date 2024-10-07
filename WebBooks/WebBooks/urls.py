26a43c6277bda82c1a7e7fbe55c3bf0a3068480b
Your Name <you@example.com>
2024-10-07 10:41:03 +0300

    07.10.2024 Сессии в Django. Аутентификация и авторизация пользователей. Создание отдельных пользователей и групп пользователей. Создание страницы регистрации пользователя при входе на сайт. Создание страницы для сброса пароля пользователя. Поверка подлинности входа пользователя в систему. Шевелева Ростовцева

--- WebBooks/WebBooks/urls.py
+++ WebBooks/WebBooks/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path
 from catalog import views
 from django.urls import  re_path
+from django.urls import path, include
 
 urlpatterns = [
     path('', views.index, name='index'),
@@ -26,4 +27,8 @@ urlpatterns = [
     re_path(r'^book/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book-detail'),
     re_path(r'^authors/$', views.AuthorListView.as_view(), name='authors'),
 
+]
+
+urlpatterns += [
+ path('accounts/', include('django.contrib.auth.urls')),
 ]
