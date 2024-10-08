b530d5fa9a3579c011e14147d3fc8beb5aa39da9
Kustikphphph <puppy.maria@yandex.ru>
2024-10-08 22:08:54 +0300

    08.10.24. Формирование страниц сайта для создания заказов на книги. Управление формами в Django. Форма для ввода и обновления информации об авторах книг на основе класса Fоrm(). Форма для ввода и обновления информации о книгах на основе класса ModelForm(). Ростовцева Шевелева

--- WebBooks/catalog/views.py
+++ WebBooks/catalog/views.py
@@ -5,6 +5,10 @@ from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
 from catalog import views
 from .forms import AuthorsForm
+from django.http import HttpResponseRedirect
+from django.views.generic.edit import CreateView, UpdateView, DeleteView 
+from django.urls import reverse_lazy 
+from .models import Book
 
 # Create your views here.
 
@@ -51,3 +55,47 @@ def authors_add(request):
     authorsform = AuthorsForm()
     return render(request, "catalog/authors_add.html",
     {"form": authorsform, "author": author})
+
+def create(request):
+    if request.method == "POST": 
+        author = Author() 
+        author.first_name = request.POST.get("first_name") 
+        author.last_name = request.POST.get("last_name") 
+        author.date_of_birth = request.POST.get("date_of_birth") 
+        author.date_of_death = request.POST.get("date_of_death") 
+        author.save() 
+        return HttpResponseRedirect("/authors_add/")
+
+def delete(request, id): 
+    try: 
+        author = Author.objects.get(id=id) 
+        author.delete() 
+        return HttpResponseRedirect("/authors_add/") 
+    except Author.DoesNotExist: 
+        return HttpResponseNotFound("<h2>Автор не найден</h2>")
+
+def edit1(request, id): 
+    author = Author.objects.get(id=id) 
+    if request.method == "POST": 
+        author.first_name = request.POST.get("first_name") 
+        author.last_name = request.POST.get("last_name") 
+        author.date_of_birth = request.POST.get("date_of_birth") 
+        author.date_of_death = request.POST.get("date_of_death") 
+        author.save() 
+        return HttpResponseRedirect("/authors_add/") 
+    else: 
+        return render(request, "edit1.html", {"author": author})
+
+class BookCreate(CreateView): 
+    model = Book 
+    fields = '__all__' 
+    success_url = reverse_lazy('books')
+
+class BookUpdate(UpdateView): 
+    model = Book 
+    fields = '__all__' 
+    success_url = reverse_lazy('books')
+
+class BookDelete(DeleteView): 
+    model = Book 
+    success_url = reverse_lazy('books')
