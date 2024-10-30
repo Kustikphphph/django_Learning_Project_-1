4e515d3aeaaeca72da53388727c35a6402e6eb35
Kustikphphph <puppy.maria@yandex.ru>
2024-10-30 23:30:09 +0300

    лз-16 Формы. Процесс управления формами в Django. Определение форм. Настройка среды для изучения полей разных типов. Типы полей в формах Django и их общие параметры. Поля BooleanField, CharField, ChoiceField, DateField, DateTimeField, DecimalField, DurationField, EmailField.

--- hello/firstapp/forms.py
+++ hello/firstapp/forms.py
@@ -6,5 +6,4 @@ from django import forms
 #     age = forms.IntegerField(label="Возраст клиента",
 #     widget=forms.NumberInput(attrs={"class": "myfield"}))
 class UserForm(forms.Form):
-     name = forms.CharField(label="Имя клиента", widget=forms.TextInput(attrs={"class": "myfield"}))
-     age = forms.IntegerField(label="Возраст клиента", widget=forms.NumberInput(attrs={"class": "myfield"}))
+     email = forms.EmailField(label="Электронный адрес", help_text="Обязательный символ - @")
--- hello/firstapp/views.py
+++ hello/firstapp/views.py
@@ -27,16 +27,9 @@ def about(request):
 def contact(request):
     return render(request, "firstapp/contact.html")
 def myform(request):
-    if request.method == "POST":
-        userform = UserForm(request.POST)
-        if userform.is_valid():
-            name = request.POST.get("name") # получить значение поля Имя
-            age = request.POST.get("age") # получить значение поля Возраст
-            output = "<h2>Пользователь</h2><h3>Имя - {0}," \
-                " Возраст – {1} </h3 >".format(name, age)
-            return HttpResponse(output)
-    userform = UserForm()
-    return render(request, "firstapp/myform.html", {"form": userform})
+    my_form = UserForm() 
+    context = {"form": my_form} 
+    return render(request, "firstapp/my_form.html", context)
 
 # alex=User.objects.create(name="Александр")
 # acc=Account(login="1234", password="6565")
